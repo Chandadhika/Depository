b1b00d815febc143811c1714b7235e476a162733
ANarngAung <anarngaung2-17@gmail.com>
2024-09-29 15:49:16 +0630

    changed javascript links

--- assets/js/test1.js
+++ assets/js/test1.js
@@ -4,3 +4,10 @@ document.getElementById('linkSelect').addEventListener('change', function() {
         window.location.href = selectedValue; // Redirect to the selected link
     }
 });
+
+function navigateToPage(selectElement) {
+    const selectedValue = selectElement.value;
+    if (selectedValue) {
+        window.location.href = selectedValue;
+    }
+}
